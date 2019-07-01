cf223cd98338bf8e84fef3054585f4f677c8bc4f
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2019-07-01 01:31:37 -0300

    updating

--- src/br/ufc/crateus/graph/App.java
+++ src/br/ufc/crateus/graph/App.java
@@ -3,7 +3,21 @@ package br.ufc.crateus.graph;
 public class App {
 
 	public static void main(String[] args) {
+		AdjacencyListGraph<String> test = new AdjacencyListGraph<String>(5);
 		
+		test.addEdge("Alfredo", "Maria");
+		test.addEdge("Alfredo", "Joana");
+		test.addEdge("Maria", "Joana");
+		test.addEdge("Marcelo", "Maria");
+		test.addEdge("Marcelo", "Alfredo");
+		
+		//System.out.println(test.contains("Marcelo"));
+		//System.out.println("N° de vértices: "+test.countVertices());
+		//System.out.println("N° de arestas: "+test.countEdges());
+		//System.out.println(test.index("Joana"));
+		//System.out.println(test.label(1));
+		//System.out.println(test.adjacents("Alfredo"));
+		System.out.println(test.degree("Maria"));
 	}
 
 }
